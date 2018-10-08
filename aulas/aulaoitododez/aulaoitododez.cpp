a6e5eb7204c668a71736b35bf6806ca93740bd63
MatheusTeixeira <matheus2611@gmail.com>
2018-10-08 11:22:18 -0300

    Strings

--- aulas/aulaoitododez/aulaoitododez.cpp
+++ aulas/aulaoitododez/aulaoitododez.cpp
@@ -16,7 +16,44 @@ bool d; //booleano(v/f)
 
 int main()
 {
-	int a; 
+	int e;
+	 
+	printf("hello world!\n");
+
+	printf("Digite seu nome:\n"); // \n = new line, \t = dar espaço
+
+	printf("%i\n", 99);
+	printf("%f\n", 100.01);
+
+
+	printf("aqui vai o valor %f, total da soma.\n", 7.5 + 5.8);
+
+	printf("%i + %i = %i\n", 500, 600, 500 + 600);
+
+	a = 120;
+	e = 210;
+	int f = a + e;
+
+	printf(" Resultado da soma: %i\n", f);
+
+	printf("Digite um numero");
+
+	scanf_s("%f", &b); // imprimir um valor
+
+	printf("\n voce digitou %f\n", b);
+
+
+
+
+		
+
+	system("pause");
+
+
+
+
+
+
     return 0;
 }
 
