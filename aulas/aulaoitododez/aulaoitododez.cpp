1e61ccfb516800566bb1e74e9fdf439c3fdddcca
MatheusTeixeira <matheus2611@gmail.com>
2018-10-08 09:39:50 -0300

    variaveis

--- aulas/aulaoitododez/aulaoitododez.cpp
+++ aulas/aulaoitododez/aulaoitododez.cpp
@@ -3,11 +3,20 @@
 
 #include "stdafx.h"
 
+#include <stdio.h> //inclusão de bliblioteca
+#include <stdlib.h>
+
+//Variaveis
+int a;
+float b; //real(ponto flutuante)
+double c; //real(mais precisão)
+bool d; //booleano(v/f)
 
 
 
 int main()
 {
+	int a; 
     return 0;
 }
 
