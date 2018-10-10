151cf7cfc055c320f93026bbedc72af2f79ee59b
Matheus2611 <43751314+Matheus2611@users.noreply.github.com>
2018-10-10 17:54:59 -0300

    Calculadorasimples

---
+++ aulas/Calculadorasimples/Calculadorasimples.cpp
@@ -0,0 +1,39 @@
+// Calculadorasimples.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdlib.h>
+
+float recebervalores()
+{
+	float valorreal;
+	printf("\tDigite um valor real:");
+	scanf_s("%f", &valorreal);
+	return valorreal;
+}
+float calcadicao(float a, float b)
+{
+	float adicao;
+	adicao = a + b;
+	return adicao
+}
+float calcsubtracao(float a, float b)
+{
+	float subtracao;
+	subtracao = a - b;
+	return subtracao
+}
+
+
+int main()
+{
+
+	float a = recebervalores();
+	float b = recebervalores();
+
+	printf("Valor da adição:\t", calcadicao(a, b), "Subtração:\t", calcsubtracao(a, b);
+	system("pause");
+	return 0;
+}
+
+
