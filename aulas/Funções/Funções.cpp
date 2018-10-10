44719bdd00b29b750a6414af66906caac177c019
MatheusTeixeira <matheus2611@gmail.com>
2018-10-10 11:02:51 -0300

    Funções

---
+++ aulas/Funções/Funções.cpp
@@ -0,0 +1,39 @@
+// Funções.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+float valoreal()
+float valorreal;
+printf("\tDigite um valor real:");
+scanf_s("%f", &valorreal);
+return valorreal;
+
+float adicao(float a, float b)
+float adicao;
+adicao = a + b;
+return adicao
+
+float subtracao(float a, float b)
+float subtracao;
+subtracao = a + b;
+return subtracao
+
+
+
+
+
+
+int main()
+{
+
+
+
+
+    return 0;
+
+
+
+}
+
+
