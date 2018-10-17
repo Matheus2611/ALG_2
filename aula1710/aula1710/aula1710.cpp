a5fb297a06d209c89ab71feab63f67b4d25d9b83
MatheusTeixeira <matheus2611@gmail.com>
2018-10-17 11:18:50 -0300

    Aula1710

---
+++ aula1710/aula1710/aula1710.cpp
@@ -0,0 +1,33 @@
+// aula1710.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdlib.h>
+
+int main()
+{
+	if (true)
+	{
+		printf("Sempre vai imprimir essa frase\n");
+	}
+	if (false
+	{
+		printf("essa frase nunca sera impressa\n");
+	}
+
+	int a = 5;
+
+	if (a > 3)
+	{
+		printf("A eh igual a 3");
+	}
+	if (a != 3)
+	{
+		printf("A eh diferente de 3");
+	}
+	if (a <= 5)
+	{
+		printf("A eh menor ou igual a 5\n")
+	}
+		return 0;
+}
