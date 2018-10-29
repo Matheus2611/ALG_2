9d738416d4b51522fa66a0a899061480cf378ac7
MatheusTeixeira <matheus2611@gmail.com>
2018-10-29 11:13:06 -0300

    aula2910_2

---
+++ aula2910/aula2910_2/aula2910_2.cpp
@@ -0,0 +1,29 @@
+// aula2910_2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdlib.h>
+
+
+
+int lervalores()
+{
+	int vet[i];
+	while (valor != -1)
+	{
+		printf("Digite um valor");
+		scanf_s("%i", &valor);
+	}
+	return valor;
+}
+
+int main()
+{
+	
+	lervalores();
+
+
+	system("pause");
+    return 0;
+}
+
